2b03e6192d75c86729dd5faeff923e442d952dca
yerbol <93880074+aruaqsyz@users.noreply.github.com>
2022-02-23 15:39:51 +0600

    Add files via upload

---
+++ Interpol/wanted.js
@@ -0,0 +1,9 @@
+if(document.images) {
+   var image1 = new Image();
+   image1.src = "rasul.png ";
+   var image2 = new Image();
+   image2.src = "rasul2.jpg";
+}
+
+var audio = new Audio("rassol.mp3");
+audio.play();
