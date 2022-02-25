e7e80ade9d5176c0bee010d5e9d079c5786a80f0
yerbol <93880074+aruaqsyz@users.noreply.github.com>
2022-02-25 14:31:29 +0600

    Add files via upload

---
+++ Interpol/js/reg.js
@@ -0,0 +1,13 @@
+  let uname = document.getElementById('uname');
+  let p;
+
+  function register(){
+    let uname = document.getElementById('uname');
+    const p1  = document.getElementById('password1');
+    const p2  = document.getElementById('password2');
+     if(p1 === p2 ){
+      alert('incorrect password')
+    }
+    else {
+      console.log('dyrus');
+  }
---
+++ Interpol/js/wanted.js
@@ -0,0 +1,10 @@
+if(document.images) {
+   var image1 = new Image();
+   image1.src = "img/rasul.png ";
+   var image2 = new Image();
+   image2.src = "img/rasul2.jpg";
+}
+
+function playSound(animal) {
+document.getElementById(animal).play();
+};
